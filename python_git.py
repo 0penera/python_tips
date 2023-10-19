fea730ce023df1dd0fa494aa69f64261433483e6
Peach <hbs7023@naver.com>
2023-10-20 06:20:31 +0900

    updated git code

--- python_git.py
+++ python_git.py
@@ -3,3 +3,6 @@ git config --global user.name " "
 git config --global user.email " "
 git config --global init.default branch main
 git restore --staged file_name
+git rebase -i --root
+git branch   
+git switch bb
